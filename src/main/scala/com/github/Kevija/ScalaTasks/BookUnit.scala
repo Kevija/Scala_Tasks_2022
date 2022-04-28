060ff02f315f0a5f3d7854b4c6fad15e7b3e7708
ek <evija.kvante@inbox.lv>
2022-04-28 21:38:14 +0300

    Parsing XML and BookUnit case class

---
+++ src/main/scala/com/github/Kevija/ScalaTasks/BookUnit.scala
@@ -0,0 +1,11 @@
+package com.github.Kevija.ScalaTasks
+
+
+case class BookUnit(id:String,
+                    author:String,
+                    title:String,
+                    genre:String,
+                    price:Double,
+                    publish_date: String,
+                    description: String)
+//TODO finish the case class with the right fields from books.xml book elements
